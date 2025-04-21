65874a2f3b86917034ff46dd8a843d5aaf7cea4c
ZebdaYacine <36519629+ZebdaYacine@users.noreply.github.com>
2025-04-21 12:50:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 let slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
-setInterval(() => {
-  slides[currentSlide].classList.remove('active');
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].classList.add('active');
-}, 4000);
+let interval = setInterval(nextSlide, 4000);
+let isPaused = false;
+
+function nextSlide() {
+  if (!isPaused) {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }
+}
+
+slides.forEach(slide => {
+  slide.addEventListener('mouseenter', () => {
+    isPaused = true; 
+  });
+
+  slide.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+      isPaused = false; 
+    }, 4000);
+  });
+});
